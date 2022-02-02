3c37a7f1a78d136f8d94f50a4b22e77604e1b21e
GitHub Action <action@github.com>
2022-02-02 20:37:45 +0000

    Template cleanup

--- src/main/kotlin/com/github/domblack/intellijplugin/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/domblack/intellijplugin/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.domblack.intellijplugin.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.domblack.intellijplugin.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/domblack/intellijplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/domblack/intellijplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.domblack.intellijplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.domblack.intellijplugin.MyBundle
 
 class MyApplicationService {
 
